056e9c1a92309491caa1a2f8539f2eb9fef8b45d
Assel Kassymzhanova <kassymzhanovaaselya@gmail.com>
2020-12-06 16:36:38 +0600

    Template review

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return 'about';
+Route::get('/index', function () {
+    return view('index');
 });
